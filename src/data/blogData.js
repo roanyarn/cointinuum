0234de337928925c42262508804676fb536fbb72
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-08-29 18:02:33 -0400

    update new blog info and picture

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,48 +1,54 @@
 import Img1 from '../images/connect/img1.jpeg';
 import Img2 from '../images/connect/img2.jpeg';
 import Img3 from '../images/connect/img3.jpg';
-import Img4 from '../images/connect/img4.jpeg'; 
+import Img4 from '../images/connect/img4.jpeg';
+import Img5 from '../images/connect/img5.png';
 
 const blogData = [
-	{
-		id: '1',
-		title: '5 Digital Asset Projects That are Tokenizing Land IRL',
-		paragraph:
-			'I’ll say it. We hear too much about crypto bros, yield farming, and bored apes. Isn’t web 3 supposed to save the internet? Where’s the revolution? This list may not be glamorous, but these projects are re-imagining...',
-		date: 'May 31, 2022',
-		image: Img4,
-		url:
-			'https://cointinuum.medium.com/5-digital-asset-projects-that-are-tokenizing-land-irl-44af6dc88453',
-	},
-	{
-		id: '2',
-		title: 'A Leap into Crypto: Russia-Ukraine War',
-		paragraph:
-			'Amid Russia’s invasion of Ukraine, dynamics between global economies and economic warfare have put a direct spotlight on digital assets. As financial institutions are put in distress, people and governments are promoting...',
-		date: 'May 30, 2022',
-		image: Img3,
-		url:
-			'https://cointinuum.medium.com/a-leap-into-crypto-russia-ukraine-war-d3cb4fdea582',
-	},
-	{
-		id: '3',
-		title: 'Spooking the Herd” PART I',
-		paragraph:
-			'In Part 1, we will understand what a Bitcoin whale is and how they can trigger a sell-off. In Part 2, we will look at strategies to counter this and indeed rely on it for increased profitability. Firstly, let’s look at the anatomy of a “whale”. This will give us a common port to cast away from...',
-		date: 'Feb 26, 2021',
-		image: Img1,
-		url: 'https://cointinuum.medium.com/spooking-the-herd-part-i-46237725f8ff',
-	},
-	{
-		id: '4',
-		title: 'How to choose a Cryptocurrency to invest?',
-		paragraph:
-			'What are your main considerations when choosing a currency to invest in? Total capitalization? Daily volume? Reddit or tiktok influencers? If I may suggest some additions factors to take into account...',
-		date: 'Feb 10, 2021',
-		image: Img2,
-		url:
-			'https://cointinuum.medium.com/how-to-choose-a-cryptocurrency-to-invest-297546a4d523',
-	},
+  {
+    id: '1',
+    title: 'How to choose a Cryptocurrency to invest?',
+    paragraph:
+      'What are your main considerations when choosing a currency to invest in? Total capitalization? Daily volume? Reddit or tiktok influencers? If I may suggest some additions factors to take into account...',
+    date: 'Feb 10, 2021',
+    image: Img2,
+    url: 'https://cointinuum.medium.com/how-to-choose-a-cryptocurrency-to-invest-297546a4d523',
+  },
+  {
+    id: '2',
+    title: 'Spooking the Herd” PART I',
+    paragraph:
+      'In Part 1, we will understand what a Bitcoin whale is and how they can trigger a sell-off. In Part 2, we will look at strategies to counter this and indeed rely on it for increased profitability. Firstly, let’s look at the anatomy of a “whale”. This will give us a common port to cast away from...',
+    date: 'Feb 26, 2021',
+    image: Img1,
+    url: 'https://cointinuum.medium.com/spooking-the-herd-part-i-46237725f8ff',
+  },
+  {
+    id: '3',
+    title: 'A Leap into Crypto: Russia-Ukraine War',
+    paragraph:
+      'Amid Russia’s invasion of Ukraine, dynamics between global economies and economic warfare have put a direct spotlight on digital assets. As financial institutions are put in distress, people and governments are promoting...',
+    date: 'May 30, 2022',
+    image: Img3,
+    url: 'https://cointinuum.medium.com/a-leap-into-crypto-russia-ukraine-war-d3cb4fdea582',
+  },
+  {
+    id: '4',
+    title: '5 Digital Asset Projects That are Tokenizing Land IRL',
+    paragraph:
+      'I’ll say it. We hear too much about crypto bros, yield farming, and bored apes. Isn’t web 3 supposed to save the internet? Where’s the revolution? This list may not be glamorous, but these projects are re-imagining...',
+    date: 'May 31, 2022',
+    image: Img4,
+    url: 'https://cointinuum.medium.com/5-digital-asset-projects-that-are-tokenizing-land-irl-44af6dc88453',
+  },
+  {
+    id: '5',
+    title: 'A Step Towards Crypto Regulation with Charles Hoskinson',
+    paragraph: `On June 23, Charles Hoskinson, founder of Cardano (ADA), testified in Congress in regards to the future of crypto regulation. Taking on the face of crypto and blockchain industries, Hoskinson detailed digital asset regulation...`,
+    date: 'Aug 24, 2022',
+    image: Img5,
+    url: 'https://cointinuum.medium.com/a-step-towards-crypto-regulation-with-charles-hoskinson-813792d45067',
+  },
 ];
 
 export default blogData;
--- src/pages/Connect/Connect.js
+++ src/pages/Connect/Connect.js
@@ -20,349 +20,351 @@ import useValidation from '../../hooks/useValidation';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 
 const Connect = () => {
-	const { darkMode } = useContext(DarkModeContext);
-	const [isEmailSent, setIsEmailSent] = useState(false);
+  const { darkMode } = useContext(DarkModeContext);
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
-	const EMAIL_MIN_SIZE = 2;
-	const EMAIL_MAX_SIZE = 100;
-	const NAME_MIN_SIZE = 2;
-	const NAME_MAX_SIZE = 50;
-	const SUBJECT_MIN_SIZE = 3;
-	const SUBJECT_MAX_SIZE = 50;
-	const DESCRIPTION_MIN_SIZE = 3;
-	const DESCRIPTION_MAX_SIZE = 50;
-	const MESSAGE_MIN_SIZE = 2;
-	const MESSAGE_MAX_SIZE = 1500;
+  const EMAIL_MIN_SIZE = 2;
+  const EMAIL_MAX_SIZE = 100;
+  const NAME_MIN_SIZE = 2;
+  const NAME_MAX_SIZE = 50;
+  const SUBJECT_MIN_SIZE = 3;
+  const SUBJECT_MAX_SIZE = 50;
+  const DESCRIPTION_MIN_SIZE = 3;
+  const DESCRIPTION_MAX_SIZE = 50;
+  const MESSAGE_MIN_SIZE = 2;
+  const MESSAGE_MAX_SIZE = 1500;
 
-	const [email, setEmail, emailError, handleEmailError] = useValidation({
-		fieldName: 'email',
-		minSize: EMAIL_MIN_SIZE,
-		maxSize: EMAIL_MAX_SIZE,
-		type: 'email',
-		required: true,
-	});
+  const [email, setEmail, emailError, handleEmailError] = useValidation({
+    fieldName: 'email',
+    minSize: EMAIL_MIN_SIZE,
+    maxSize: EMAIL_MAX_SIZE,
+    type: 'email',
+    required: true,
+  });
 
-	const [name, setName, nameError, handleNameError] = useValidation({
-		fieldName: 'name',
-		minSize: NAME_MIN_SIZE,
-		maxSize: NAME_MAX_SIZE,
-		type: 'letters',
-		required: true,
-	});
+  const [name, setName, nameError, handleNameError] = useValidation({
+    fieldName: 'name',
+    minSize: NAME_MIN_SIZE,
+    maxSize: NAME_MAX_SIZE,
+    type: 'letters',
+    required: true,
+  });
 
-	const [subject, setSubject, subjectError, handleSubjectError] = useValidation(
-		{
-			fieldName: 'subject',
-			minSize: SUBJECT_MIN_SIZE,
-			maxSize: SUBJECT_MAX_SIZE,
-			type: 'letters',
-			required: true,
-		}
-	);
+  const [subject, setSubject, subjectError, handleSubjectError] = useValidation(
+    {
+      fieldName: 'subject',
+      minSize: SUBJECT_MIN_SIZE,
+      maxSize: SUBJECT_MAX_SIZE,
+      type: 'letters',
+      required: true,
+    }
+  );
 
-	const [
-		description,
-		setDescription,
-		descriptionError,
-		handleDescriptionError,
-	] = useValidation({
-		fieldName: 'description',
-		minSize: DESCRIPTION_MIN_SIZE,
-		maxSize: DESCRIPTION_MAX_SIZE,
-		type: 'letters',
-		required: true,
-	});
+  const [
+    description,
+    setDescription,
+    descriptionError,
+    handleDescriptionError,
+  ] = useValidation({
+    fieldName: 'description',
+    minSize: DESCRIPTION_MIN_SIZE,
+    maxSize: DESCRIPTION_MAX_SIZE,
+    type: 'letters',
+    required: true,
+  });
 
-	const [message, setMessage, messageError, handleMessageError] = useValidation(
-		{
-			fieldName: 'message',
-			minSize: MESSAGE_MIN_SIZE,
-			maxSize: MESSAGE_MAX_SIZE,
-			type: 'all',
-			required: true,
-		}
-	);
+  const [message, setMessage, messageError, handleMessageError] = useValidation(
+    {
+      fieldName: 'message',
+      minSize: MESSAGE_MIN_SIZE,
+      maxSize: MESSAGE_MAX_SIZE,
+      type: 'all',
+      required: true,
+    }
+  );
 
-	const onEmailChange = (e) => setEmail(e.target.value);
-	const onNameChange = (e) => setName(e.target.value);
-	const onSubjectChange = (e) => setSubject(e.target.value);
-	const onDescriptionChange = (e) => setDescription(e.target.value);
-	const onMessageChange = (e) => setMessage(e.target.value);
+  const onEmailChange = (e) => setEmail(e.target.value);
+  const onNameChange = (e) => setName(e.target.value);
+  const onSubjectChange = (e) => setSubject(e.target.value);
+  const onDescriptionChange = (e) => setDescription(e.target.value);
+  const onMessageChange = (e) => setMessage(e.target.value);
 
-	useEffect(() => {
-		const getEmail = () => {
-			if (isEmailSent) {
-				setTimeout(() => {
-					setIsEmailSent(false);
-				}, 10000);
-			}
-		};
-		getEmail();
-	}, [isEmailSent]);
+  useEffect(() => {
+    const getEmail = () => {
+      if (isEmailSent) {
+        setTimeout(() => {
+          setIsEmailSent(false);
+        }, 10000);
+      }
+    };
+    getEmail();
+  }, [isEmailSent]);
 
-	const sendEmail = (e) => {
-		e.preventDefault();
-		if (!email || !name || !subject || !description || !message) {
-			handleEmailError();
-			handleNameError();
-			handleSubjectError();
-			handleDescriptionError();
-			handleMessageError();
-			return;
-		}
-		if (
-			!(
-				emailError ||
-				nameError ||
-				subjectError ||
-				descriptionError ||
-				messageError
-			)
-		) {
-			setIsEmailSent(true);
-			emailjs
-				.sendForm(
-					'service_2a5rols',
-					'template_dm0k2b3',
-					e.target,
-					'0NbLq2er2L3Kmif5k'
-				)
-				.then(
-					(result) => {
-						console.log(result.text);
-					},
-					(error) => {
-						console.log(error.text);
-					}
-				);
-			setEmail('');
-			setName('');
-			setSubject('');
-			setDescription('');
-			setMessage('');
-		}
-	};
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (!email || !name || !subject || !description || !message) {
+      handleEmailError();
+      handleNameError();
+      handleSubjectError();
+      handleDescriptionError();
+      handleMessageError();
+      return;
+    }
+    if (
+      !(
+        emailError ||
+        nameError ||
+        subjectError ||
+        descriptionError ||
+        messageError
+      )
+    ) {
+      setIsEmailSent(true);
+      emailjs
+        .sendForm(
+          'service_2a5rols',
+          'template_dm0k2b3',
+          e.target,
+          '0NbLq2er2L3Kmif5k'
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      setEmail('');
+      setName('');
+      setSubject('');
+      setDescription('');
+      setMessage('');
+    }
+  };
 
-	const onBlogClick = (url) => {
-		window.open(url, '_blank');
-	};
+  const onBlogClick = (url) => {
+    window.open(url, '_blank');
+  };
 
-	return (
-		<div className={darkMode ? 'connect connect__dark' : 'connect'}>
-			<Title
-				title="Join the Cointinuum Community"
-				paragraph="Cointinuum Connect is a vibrant hub for real estate professionals, community investors, crypto holders, and believers alike! Looking to learn more about CTM and BRX? Connect with our community today."
-			/>
-			<div
-				className={
-					darkMode
-						? 'connect__image--container connect__image--container--dark'
-						: 'connect__image--container'
-				}
-			>
-				{socialMediaData.map(({ id, name, url, image }) => {
-					return (
-						<a key={id} href={url} target="_blank" rel="noreferrer">
-							<img
-								className={
-									darkMode
-										? 'connect__image connect__image--dark'
-										: 'connect__image'
-								}
-								src={image}
-								alt={name}
-							/>
-						</a>
-					);
-				})}
-			</div>
-			<h1 className="connect__title">The Latest from Our Blog</h1>
-			<Swiper
-				slidesPerView={3}
-				spaceBetween={10}
-				navigation={true}
-				pagination={{ clickable: true }}
-				breakpoints={{
-					310: {
-						slidesPerView: 1,
-						spaceBetween: 20,
-					},
-					650: {
-						slidesPerView: 2,
-						spaceBetween: 30,
-					},
-					1100: {
-						slidesPerView: 3,
-						spaceBetween: -115,
-					},
-					1600: { 
-						slidesPerView: 4,
-						spaceBetween: -115,
-					}, 
-				}}
-				modules={[Navigation, Pagination]}
-				className="mySwiper"
-			>
-				{blogData.map(({ id, title, paragraph, date, image, url }) => {
-					return (
-						<SwiperSlide>
-							<BlogCard
-								key={id}
-								title={title}
-								paragraph={paragraph}
-								date={date}
-								image={image}
-								onClick={() => onBlogClick(url)}
-							/>
-						</SwiperSlide>
-					);
-				})}
-			</Swiper>
-			<h1 className="connect__title">Upcoming Events</h1>
-			<Swiper
-				slidesPerView={3}
-				spaceBetween={10}
-				navigation={true}
-				pagination={{ clickable: true }}
-				breakpoints={{
-					310: {
-						slidesPerView: 1,
-						spaceBetween: 20,
-					},
-					650: {
-						slidesPerView: 2,
-						spaceBetween: 10,
-					},
-					1020: {
-						slidesPerView: 4,
-						spaceBetween: -100,
-					},
-				}}
-				modules={[Navigation, Pagination]}
-				className="mySwiper"
-			>
-				{eventsData.map(({ id, title, subTitle, time, day, month, image }) => {
-					return (
-						<SwiperSlide>
-							<EventCard
-								key={id}
-								title={title}
-								subTitle={subTitle}
-								day={day}
-								month={month}
-								time={time}
-								image={image}
-							/>
-						</SwiperSlide>
-					);
-				})}
-			</Swiper>
-			<Title title="Send Us a Message" />
-			{!isEmailSent ? (
-				<form className="connect__form" onSubmit={sendEmail}>
-					<input
-						name="email"
-						className={
-							darkMode
-								? 'connect__input connect__input--dark'
-								: 'connect__input'
-						}
-						type="email"
-						placeholder="Email"
-						value={email}
-						onBlur={handleEmailError}
-						onChange={onEmailChange}
-					/>
-					{emailError && <ErrorMessage>{emailError}</ErrorMessage>}
-					<div className="connect__name">
-						<div className="input__name">
-							<input
-								name="name"
-								className={
-									darkMode
-										? 'connect__input connect__input--dark'
-										: 'connect__input'
-								}
-								type="text"
-								placeholder="Name"
-								value={name}
-								onBlur={handleNameError}
-								onChange={onNameChange}
-							/>
-							{nameError && <ErrorMessage>{nameError}</ErrorMessage>}
-						</div>
-						<div className="input__name">
-							<input
-								name="subject"
-								className={
-									darkMode
-										? 'connect__input connect__input--dark'
-										: 'connect__input'
-								}
-								type="text"
-								placeholder="Subject"
-								value={subject}
-								onBlur={handleSubjectError}
-								onChange={onSubjectChange}
-							/>
-							{subjectError && <ErrorMessage>{subjectError}</ErrorMessage>}
-						</div>
-					</div>
-					<input
-						name="description"
-						className={
-							darkMode
-								? 'connect__input connect__input--dark'
-								: 'connect__input'
-						}
-						type="text"
-						placeholder="Who are you? (eg. enthusiast, real estate agent, nonprofit organization, etc.)"
-						value={description}
-						onBlur={handleDescriptionError}
-						onChange={onDescriptionChange}
-					/>
-					{descriptionError && <ErrorMessage>{descriptionError}</ErrorMessage>}
-					<textarea
-						name="message"
-						className={
-							darkMode
-								? 'connect__input connect__input--dark'
-								: 'connect__input'
-						}
-						cols="30"
-						rows="10"
-						placeholder="Message"
-						value={message}
-						onBlur={handleMessageError}
-						onChange={onMessageChange}
-					/>
-					{messageError && <ErrorMessage>{messageError}</ErrorMessage>}
-					<p>
-						If you would like to recive periodic communications from Cointinuum
-						about upcoming events, product launches, airdrops, and more, please
-						check the box below:
-					</p>
-					<p>
-						<input type="checkbox" className="connect__checkbox"></input>
-						<label form="connect__checkbox">
-							I agree to recieve newsletters from Cointinuum and the Bit Real
-							Estate Exchange.
-						</label>
-					</p>
-					<Button
-						className="btn__secondary"
-						content="Send"
-						content2="Send"
-						type="submit"
-					/>
-				</form>
-			) : (
-				<h1 className="connect__message--success">
-					We have received your message.
-				</h1>
-			)}
-		</div>
-	);
+  return (
+    <div className={darkMode ? 'connect connect__dark' : 'connect'}>
+      <Title
+        title="Join the Cointinuum Community"
+        paragraph="Cointinuum Connect is a vibrant hub for real estate professionals, community investors, crypto holders, and believers alike! Looking to learn more about CTM and BRX? Connect with our community today."
+      />
+      <div
+        className={
+          darkMode
+            ? 'connect__image--container connect__image--container--dark'
+            : 'connect__image--container'
+        }
+      >
+        {socialMediaData.map(({ id, name, url, image }) => {
+          return (
+            <a key={id} href={url} target="_blank" rel="noreferrer">
+              <img
+                className={
+                  darkMode
+                    ? 'connect__image connect__image--dark'
+                    : 'connect__image'
+                }
+                src={image}
+                alt={name}
+              />
+            </a>
+          );
+        })}
+      </div>
+      <h1 className="connect__title">The Latest from Our Blog</h1>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        navigation={true}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          310: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          650: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1100: {
+            slidesPerView: 3,
+            spaceBetween: -115,
+          },
+          1600: {
+            slidesPerView: 4,
+            spaceBetween: -115,
+          },
+        }}
+        modules={[Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {blogData
+          .reverse()
+          .map(({ id, title, paragraph, date, image, url }) => {
+            return (
+              <SwiperSlide>
+                <BlogCard
+                  key={id}
+                  title={title}
+                  paragraph={paragraph}
+                  date={date}
+                  image={image}
+                  onClick={() => onBlogClick(url)}
+                />
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+      <h1 className="connect__title">Upcoming Events</h1>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        navigation={true}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          310: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          650: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          1020: {
+            slidesPerView: 4,
+            spaceBetween: -100,
+          },
+        }}
+        modules={[Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {eventsData.map(({ id, title, subTitle, time, day, month, image }) => {
+          return (
+            <SwiperSlide>
+              <EventCard
+                key={id}
+                title={title}
+                subTitle={subTitle}
+                day={day}
+                month={month}
+                time={time}
+                image={image}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <Title title="Send Us a Message" />
+      {!isEmailSent ? (
+        <form className="connect__form" onSubmit={sendEmail}>
+          <input
+            name="email"
+            className={
+              darkMode
+                ? 'connect__input connect__input--dark'
+                : 'connect__input'
+            }
+            type="email"
+            placeholder="Email"
+            value={email}
+            onBlur={handleEmailError}
+            onChange={onEmailChange}
+          />
+          {emailError && <ErrorMessage>{emailError}</ErrorMessage>}
+          <div className="connect__name">
+            <div className="input__name">
+              <input
+                name="name"
+                className={
+                  darkMode
+                    ? 'connect__input connect__input--dark'
+                    : 'connect__input'
+                }
+                type="text"
+                placeholder="Name"
+                value={name}
+                onBlur={handleNameError}
+                onChange={onNameChange}
+              />
+              {nameError && <ErrorMessage>{nameError}</ErrorMessage>}
+            </div>
+            <div className="input__name">
+              <input
+                name="subject"
+                className={
+                  darkMode
+                    ? 'connect__input connect__input--dark'
+                    : 'connect__input'
+                }
+                type="text"
+                placeholder="Subject"
+                value={subject}
+                onBlur={handleSubjectError}
+                onChange={onSubjectChange}
+              />
+              {subjectError && <ErrorMessage>{subjectError}</ErrorMessage>}
+            </div>
+          </div>
+          <input
+            name="description"
+            className={
+              darkMode
+                ? 'connect__input connect__input--dark'
+                : 'connect__input'
+            }
+            type="text"
+            placeholder="Who are you? (eg. enthusiast, real estate agent, nonprofit organization, etc.)"
+            value={description}
+            onBlur={handleDescriptionError}
+            onChange={onDescriptionChange}
+          />
+          {descriptionError && <ErrorMessage>{descriptionError}</ErrorMessage>}
+          <textarea
+            name="message"
+            className={
+              darkMode
+                ? 'connect__input connect__input--dark'
+                : 'connect__input'
+            }
+            cols="30"
+            rows="10"
+            placeholder="Message"
+            value={message}
+            onBlur={handleMessageError}
+            onChange={onMessageChange}
+          />
+          {messageError && <ErrorMessage>{messageError}</ErrorMessage>}
+          <p>
+            If you would like to recive periodic communications from Cointinuum
+            about upcoming events, product launches, airdrops, and more, please
+            check the box below:
+          </p>
+          <p>
+            <input type="checkbox" className="connect__checkbox"></input>
+            <label form="connect__checkbox">
+              I agree to recieve newsletters from Cointinuum and the Bit Real
+              Estate Exchange.
+            </label>
+          </p>
+          <Button
+            className="btn__secondary"
+            content="Send"
+            content2="Send"
+            type="submit"
+          />
+        </form>
+      ) : (
+        <h1 className="connect__message--success">
+          We have received your message.
+        </h1>
+      )}
+    </div>
+  );
 };
 
 export default Connect;
