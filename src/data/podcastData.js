93c4f6e421b37ae8fbcfb9c900d811063cf37cf9
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-09-05 20:27:21 -0400

    create podcast data file

---
+++ src/data/podcastData.js
@@ -0,0 +1,12 @@
+import Episode1 from '../images/podcast/episode1.png';
+
+const podcastData = [
+  {
+    id: '1',
+    title: 'Episode 1',
+    subject: 'Satoshi Noobie',
+    image: Episode1,
+    url: 'https://cointinuum.medium.com/how-to-choose-a-cryptocurrency-to-invest-297546a4d523',
+  },
+];
+export default podcastData;
