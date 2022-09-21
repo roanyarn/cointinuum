bd6b71b48472413cee97fcb5d5a2c02190a347e5
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-09-20 20:38:13 -0400

    test fetch faq data

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -1,29 +1,57 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { DarkModeContext } from '../../Context/DarkModeContext';
 import QuestionCard from '../../components/QuestionCard/QuestionCard';
 import faqData from '../../data/faqData';
+import service from '../../services/services';
 import './FAQ.scss';
 
 const FAQ = () => {
-	const { darkMode } = useContext(DarkModeContext);
-	return (
-		<div className={darkMode ? 'page page--dark' : 'page'}>
-			{faqData.map(({ id, question, answer }) => {
-				return (
-					<QuestionCard
-						key={id}
-						className={
-							darkMode
-								? 'question__card question__card--dark'
-								: 'question__card'
-						}
-						title={question}
-						paragraph={answer}
-					/>
-				);
-			})}
-		</div>
-	);
+  const { darkMode } = useContext(DarkModeContext);
+  const [faq, setFaq] = useState([]);
+
+  const getFaqs = () => {
+    service.Faq.getAllFaqs().then(({ msg }) => {
+      console.log(msg);
+      setFaq(msg);
+    });
+  };
+
+  useEffect(() => {
+    getFaqs();
+  }, []);
+
+  return (
+    <div className={darkMode ? 'page page--dark' : 'page'}>
+      {faq?.map(({ _id, question, answer }) => {
+        return (
+          <QuestionCard
+            key={_id}
+            className={
+              darkMode
+                ? 'question__card question__card--dark'
+                : 'question__card'
+            }
+            title={question}
+            paragraph={answer}
+          />
+        );
+      })}
+      {faqData.map(({ id, question, answer }) => {
+        return (
+          <QuestionCard
+            key={id}
+            className={
+              darkMode
+                ? 'question__card question__card--dark'
+                : 'question__card'
+            }
+            title={question}
+            paragraph={answer}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default FAQ;
