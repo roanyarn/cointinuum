d51b71eee9f1b868da1ff0f87aa7aaa0a7e69796
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-09-22 20:54:53 -0400

    add login to routes

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -6,24 +6,26 @@ import Layout from '../components/Layout/Layout';
 import Connect from '../pages/Connect/Connect';
 import Invest from '../pages/Invest/Invest';
 import FAQ from '../pages/FAQ/FAQ';
+import Login from '../pages/Auth/Login';
 
 const AppRouter = () => {
-	return (
-		//<BrowserRouter>
-		<HashRouter>
-			<Layout>
-				<Routes>
-					<Route path="/*" element={<HomePage />} />
-					<Route path="/solutions" element={<Solutions />} />
-					<Route path="/investments" element={<Invest />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/connect" element={<Connect />} />
-					<Route path="/FAQ" element={<FAQ />} />
-				</Routes>
-			</Layout>
-		</HashRouter>
-		//</BrowserRouter>
-	);
+  return (
+    //<BrowserRouter>
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/*" element={<HomePage />} />
+          <Route path="/solutions" element={<Solutions />} />
+          <Route path="/investments" element={<Invest />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
+    //</BrowserRouter>
+  );
 };
 
 export default AppRouter;
