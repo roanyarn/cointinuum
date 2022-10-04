2f5b9fc0ea45713da1196bd00bb3931bde760c8e
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-10-04 19:02:59 -0400

    create bounty data file

---
+++ src/data/bountyData.js
@@ -0,0 +1,62 @@
+const bountyData = [
+  {
+    id: '1',
+    question: 'This is the bounty data',
+    answer:
+      "Tokenized real estate is a digital asset which represents a physical 'Hard' asset. It can be either a direct representation of ownership in a physical asset or a non-direct representation of ownership.",
+  },
+  {
+    id: '2',
+    question:
+      'What makes Tokenized real estate better than traditional real estate?',
+    answer:
+      'For some investors, the hassles, expenses, skills and time requirements associated with being a landlord is too much to overcome. If you’re in this category of real estate investors then owning a digital asset is better for many reasons.',
+  },
+  {
+    id: '3',
+    question: 'Who can invest in Cointinuum? ',
+    answer:
+      'Generally speaking there are limitations about who can invest in Cointinuum at this stage of our development. Contact us to find out if you’re eligible. Note: Cointinuum and BRX Exchange do NOT offer investment advice.',
+  },
+  {
+    id: '4',
+    question: 'How much do I need to invest?',
+    answer: 'There’s no minimum investment requirement.',
+  },
+  {
+    id: '5',
+    question: 'How can I purchase Cointinuum tokens?',
+    answer: 'Contact us to learn more about purchasing tokens.',
+  },
+  {
+    id: '6',
+    question: 'Can I sell my tokens?',
+    answer:
+      'Following our Initial Coin Offering, token holders may sell tokens at will.',
+  },
+  {
+    id: '7',
+    question: 'What can I do with my tokens?',
+    answer: 'Sell, Trade, or Hold.',
+  },
+  {
+    id: '8',
+    question: 'What properties are in Cointinuum’s portfolio?',
+    answer:
+      'Cointinuum’s portfolio is ever-changing, balancing and rebalancing, so we do not provide this information, except we do maintain that proceeds from our tokens support property acquisitions, management and all aspects of our company infrastructure.',
+  },
+  {
+    id: '9',
+    question: 'How are properties chosen?',
+    answer:
+      'We acquire properties that align with our mission and monetary policy, to maintain the stability of Cointinuum.',
+  },
+  {
+    id: '10',
+    question:
+      'I own real estate, how can I list my property on the BRX exchange?',
+    answer: 'Contact us at listing@brx.exchange.',
+  },
+];
+
+export default bountyData;
