36bfdb9bcbced44b0ea7e6ee8081f86dbb0adc61
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-10-15 11:44:31 -0400

    update podcast image

--- src/data/podcastData.js
+++ src/data/podcastData.js
@@ -1,12 +1,12 @@
-import Episode1 from '../images/podcast/episode1.png';
+import Image1 from '../images/podcast/Image1.png';
 
 const podcastData = [
   {
     id: '1',
-    title: 'Episode 1',
-    subject: 'Coming soon!!!',
-    image: Episode1,
-    url: 'https://cointinuum.medium.com/how-to-choose-a-cryptocurrency-to-invest-297546a4d523',
+    title: 'Episode 0',
+    subject: 'No-show interview turns into real estate agent podcast',
+    image: Image1,
+    url: 'https://www.youtube.com/watch?v=K7m9Gy1BaB4',
   },
 ];
 export default podcastData;
