b4461b4aece15b5e47ef808cf18448367c1e4857
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-10-15 11:44:22 -0400

    update event cards

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,3 +1,11 @@
+import Image1 from '../images/event/event1.png';
+import Image2 from '../images/event/event2.png';
+import Image3 from '../images/event/Image3.png';
+import Image4 from '../images/event/Image4.png';
+import Image5 from '../images/event/Image5.png';
+import Image10 from '../images/event/Image10.png';
+import Image9 from '../images/event/Image9.png';
+
 const eventData = [
   {
     id: '1',
@@ -5,99 +13,93 @@ const eventData = [
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
     day: '26',
-    month: 'SEP',
-    image:
-      'https://www.drool-art.com/wp-content/uploads/2021/08/Chrome-Heart-by-Paulina-Almira-pink-XL-illustration-poster-550x770.jpg',
+    month: 'OCT',
+    image: Image1,
   },
   {
     id: '2',
     title: 'WEBINAR',
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
-    day: '03',
-    month: 'OCT',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUnLOmePSS0EUzszBfPQSSqr3V32Uz0qH63bYv4DIgCfs_X8ybCwC6GOCfY6z662mUFVY&usqp=CAU',
+    day: '09',
+    month: 'NOV',
+    image: Image2,
   },
   {
     id: '3',
     title: 'WEBINAR',
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
-    day: '10',
-    month: 'OCT',
-    image:
-      'https://www.drool-art.com/wp-content/uploads/2021/08/The-Persistence-of-Pink-by-Paulina-Almira-550x778.jpg',
+    day: '23',
+    month: 'NOV',
+    image: Image3,
   },
   {
     id: '4',
     title: 'WEBINAR',
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
-    day: '17',
-    month: 'OCT',
-    image:
-      'https://c8.alamy.com/compes/bm3nnx/estados-unidos-de-america-nueva-york-ciudad-de-nueva-york-diner-en-midtown-manhattan-bm3nnx.jpg',
+    day: '07',
+    month: 'DEC',
+    image: Image4,
   },
   {
     id: '5',
     title: 'WEBINAR',
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
-    day: '24',
-    month: 'OCT',
-    image:
-      'https://www.interactivebrokers.hu/images/web/cryptocurrency-hero.jpg',
+    day: '21',
+    month: 'DEC',
+    image: Image5,
   },
   {
     id: '6',
     title: 'WEBINAR',
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
-    day: '31',
-    month: 'OCT',
-    image:
-      'https://time.com/nextadvisor/wp-content/uploads/2021/11/Common-crypto-scams-to-watch-out-for-884x584.jpg',
+    day: '04',
+    month: 'JAN',
+    image: Image10,
   },
   {
     id: '7',
     title: 'WEBINAR',
     subTitle: 'Cointinuum Connect',
     time: '5:30 PM EST',
-    day: '07',
-    month: 'NOV',
-    image: 'https://images.mktw.net/im-429485?width=700&height=487',
+    day: '18',
+    month: 'JAN',
+    image: Image9,
   },
-  {
-    id: '8',
-    title: 'WEBINAR',
-    subTitle: 'Cointinuum Connect',
-    time: '5:30 PM EST',
-    day: '14',
-    month: 'NOV',
-    image:
-      'https://time.com/nextadvisor/wp-content/uploads/2022/02/How-Dollar-Cost-Averaging-Can-Power-Your-Crypto-Investing-Strategy.jpg',
-  },
-  {
-    id: '9',
-    title: 'WEBINAR',
-    subTitle: 'Cointinuum Connect',
-    time: '5:30 PM EST',
-    day: '21',
-    month: 'NOV',
-    image:
-      'https://static.euronews.com/articles/stories/05/53/11/48/400x225_cmsv2_237887fc-28e2-54b1-baee-3cd919ae7ec8-5531148.jpg',
-  },
-  {
-    id: '10',
-    title: 'WEBINAR',
-    subTitle: 'Cointinuum Connect',
-    time: '5:30 PM EST',
-    day: '28',
-    month: 'NOV',
-    image:
-      'https://mwi.usma.edu/wp-content/uploads/2020/08/blockchain-orange-1200x640.jpg',
-  },
+  // {
+  //   id: '8',
+  //   title: 'WEBINAR',
+  //   subTitle: 'Cointinuum Connect',
+  //   time: '5:30 PM EST',
+  //   day: '14',
+  //   month: 'NOV',
+  //   image:
+  //     'https://time.com/nextadvisor/wp-content/uploads/2022/02/How-Dollar-Cost-Averaging-Can-Power-Your-Crypto-Investing-Strategy.jpg',
+  // },
+  // {
+  //   id: '9',
+  //   title: 'WEBINAR',
+  //   subTitle: 'Cointinuum Connect',
+  //   time: '5:30 PM EST',
+  //   day: '21',
+  //   month: 'NOV',
+  //   image:
+  //     'https://static.euronews.com/articles/stories/05/53/11/48/400x225_cmsv2_237887fc-28e2-54b1-baee-3cd919ae7ec8-5531148.jpg',
+  // },
+  // {
+  //   id: '10',
+  //   title: 'WEBINAR',
+  //   subTitle: 'Cointinuum Connect',
+  //   time: '5:30 PM EST',
+  //   day: '28',
+  //   month: 'NOV',
+  //   image:
+  //     'https://mwi.usma.edu/wp-content/uploads/2020/08/blockchain-orange-1200x640.jpg',
+  // },
 ];
 
 export default eventData;
