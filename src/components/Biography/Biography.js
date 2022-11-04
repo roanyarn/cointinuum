f8023c0cde81517da8f273e0b390742850265d3f
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-11-03 20:29:19 -0400

    update biography

--- src/components/Biography/Biography.js
+++ src/components/Biography/Biography.js
@@ -5,12 +5,10 @@ import { GoLinkExternal } from 'react-icons/go';
 import { DarkModeContext } from '../../Context/DarkModeContext';
 import useClickOutside from '../../hooks/useClickOutside';
 import './Biography.scss';
-import CommonButton from '../CommonButton/CommonButton';
 
 const Biography = ({ onClick, teamBio, setBiography }) => {
   const { darkMode } = useContext(DarkModeContext);
   const domNode = useClickOutside(() => setBiography(false));
-  const token = localStorage.getItem('token');
 
   const {
     id,
@@ -62,16 +60,6 @@ const Biography = ({ onClick, teamBio, setBiography }) => {
           <p className="biography__paragraph">{description}</p>
           <p className="biography__paragraph">{quote}</p>
         </div>
-        {token && (
-          <div>
-            <CommonButton className="common__btn--primary btn__bio--update">
-              Update
-            </CommonButton>
-            <CommonButton className="common__btn--secondary btn__bio--delete">
-              Delete
-            </CommonButton>
-          </div>
-        )}
       </div>
     </div>
   );
