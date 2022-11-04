5d82e57e6fad13e3768e51f7dc6f9f1b0e6b8b4b
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-11-03 20:29:48 -0400

    delete not used imports

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { DarkModeContext } from '../../Context/DarkModeContext';
 import services from '../../services/services';
 import useValidation from '../../hooks/useValidation';
