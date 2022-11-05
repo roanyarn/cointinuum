3f320172ae534c5cdbcd3db4437ecb4d5c090ea2
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-11-05 14:59:35 -0400

    update social media data with cloud images

--- src/data/socialMediaData.js
+++ src/data/socialMediaData.js
@@ -1,54 +1,53 @@
-import Twitter from '../images/socialMedia/twitter.png';
-import Discord from '../images/socialMedia/discord.png';
-import Reddit from '../images/socialMedia/reddit.png';
-import Instagram from '../images/socialMedia/instagram.png';
-import Telegram from '../images/socialMedia/telegram.png';
-import LinkedIn from '../images/socialMedia/linkedIn.png';
-import Rarible from '../images/socialMedia/rarible.png';
-
 const socialMediaData = [
-	{
-		id: 1,
-		name: 'Twitter',
-		url: 'https://twitter.com/CointinuumBRX',
-		image: Twitter,
-	},
-	{
-		id: 2,
-		name: 'Discord',
-		url: 'https://discord.gg/YcX34rvYyD',
-		image: Discord,
-	},
-	{
-		id: 3,
-		name: 'Reddit',
-		url: 'https://www.reddit.com/r/cointinuum/',
-		image: Reddit,
-	},
-	{
-		id: 4,
-		name: 'Instagram',
-		url: 'https://www.instagram.com/cointinuum/',
-		image: Instagram,
-	},
-	{
-		id: 5,
-		name: 'Telegram',
-		url: 'https://t.me/cointinuum',
-		image: Telegram,
-	},
-	{
-		id: 6,
-		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/company/bit-real-estate-exchange',
-		image: LinkedIn,
-	},
-	{
-		id: 7,
-		name: 'Rarible',
-		url: 'https://rarible.com/cointinuum/sale',
-		image: Rarible,
-	},
+  {
+    id: 1,
+    name: 'Twitter',
+    url: 'https://twitter.com/CointinuumBRX',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673796/socialMedia/twitter_f86vyb.png',
+  },
+  {
+    id: 2,
+    name: 'Discord',
+    url: 'https://discord.gg/YcX34rvYyD',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673796/socialMedia/discord_o7rwbb.png',
+  },
+  {
+    id: 3,
+    name: 'Reddit',
+    url: 'https://www.reddit.com/r/cointinuum/',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673796/socialMedia/reddit_f7c1zg.png',
+  },
+  {
+    id: 4,
+    name: 'Instagram',
+    url: 'https://www.instagram.com/cointinuum/',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673796/socialMedia/instagram_qmxlxl.png',
+  },
+  {
+    id: 5,
+    name: 'Telegram',
+    url: 'https://t.me/cointinuum',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673797/socialMedia/telegram_hnojjh.png',
+  },
+  {
+    id: 6,
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/company/bit-real-estate-exchange',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673796/socialMedia/linkedIn_ibprzc.png',
+  },
+  {
+    id: 7,
+    name: 'Rarible',
+    url: 'https://rarible.com/cointinuum/sale',
+    image:
+      'https://res.cloudinary.com/dtzesssqo/image/upload/v1667673796/socialMedia/rarible_tvqkxe.png',
+  },
 ];
 
 export default socialMediaData;
